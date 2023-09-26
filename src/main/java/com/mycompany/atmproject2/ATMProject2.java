f6a06bf511dd7dc5f300b78e1f8cae6193e60571
txbrown <txbrown@GL13795.mass.local>
2023-09-26 16:29:09 +0100

    start local

---
+++ src/main/java/com/mycompany/atmproject2/ATMProject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atmproject2;
+
+/**
+ *
+ * @author txbrown
+ */
+public class ATMProject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
